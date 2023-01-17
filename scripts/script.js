3f0b2f56e220b8e062fee62d5544f98d09947ce7
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-17 19:04:00 -0300

    finished mobile version

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,43 @@
-menuIcon,addEventListener("click", menuDisplay)
+menuIcon.addEventListener("click", menuDisplay);
 
-let menuTop = document.querySelector('.menu_top')
+let menuTop = document.querySelector(".menu_top");
 
-function menuDisplay () {
-    if (window.getComputedStyle(menuTop).display == 'none') {
-        menuTop.style.display = 'grid'
-    } else {
-        menuTop.style.display = 'none'
-    }
+menuIcon.style.transition = "opacity 0.5s";
+
+function menuDisplay() {
+  if (window.getComputedStyle(menuTop).display == "none") {
+    menuTop.style.display = "grid";
+    menuIcon.style.opacity = 0;
+    setTimeout(() => {
+      menuIcon.src = "images/icon-close.svg";
+      menuIcon.style.opacity = 1;
+    }, 500);
+  } else {
+    menuTop.style.display = "none";
+    menuIcon.style.opacity = 0;
+    setTimeout(() => {
+      menuIcon.src = "images/icon-hamburger.svg";
+      menuIcon.style.opacity = 1;
+    }, 500);
+  }
+}
+
+let menuHeaders = document.querySelectorAll(".menuHeader");
+
+let subMenus = document.querySelectorAll(".sub_menu");
+
+let arrows = document.querySelectorAll(".arrow");
+
+menuHeaders.forEach((menuHeader, index) =>
+  menuHeader.addEventListener("click", () => toggle_submenu(index))
+);
+
+function toggle_submenu(i) {
+  if (window.getComputedStyle(subMenus[i]).display == "none") {
+    subMenus[i].style.display = "block";
+    arrows[i].style.transform = "rotate(180deg)";
+  } else {
+    subMenus[i].style.display = "none";
+    arrows[i].style.transform = "rotate(0deg)";
+  }
 }
