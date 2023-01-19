7949752a39e1bc6eb82ed59c053621aa2124ec11
Felipe Thomé <119542554+felipetn1989@users.noreply.github.com>
2023-01-19 06:41:51 -0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ let menuTop = document.querySelector(".menu_top");
 
 menuIcon.style.transition = "opacity 0.5s";
 
+/* I added a setTimeout function to create a transition effect when changing from the hamburger to the X icon. This function will perform certain lines of code after a certain amount of time (300ms in this case). THe addEventListener above will call the function menuDisplay everytime I click on the menu icon */
+
 function menuDisplay() {
   if (window.getComputedStyle(menuTop).display == "none") {
     menuTop.style.display = "grid";
@@ -21,6 +23,8 @@ function menuDisplay() {
     }, 300);
   }
 }
+
+/* This second part refers to the submenus. I used querySelectorAll to create an array of all the elements of the submenu I'm interested at. The forEach command will run through every single instance of them, and asign a function everytime I click on each element. This has an anonymous arrow function calling the toggle function that will open and close the sub-menus */
 
 let menuHeaders = document.querySelectorAll(".menuHeader");
 
